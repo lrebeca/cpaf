ffac61728ca454f6a30ffac2bfc9c2b2f9067fe6
lrebeca <rebecaloza26@gmail.com>
2022-10-05 00:08:14 -0400

    env eliminado del gitignore

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,8 +1,9 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 
     @extends('layouts.header')
     <link rel="shortcut icon" href="{{asset('favicons/favicon.ico')}}" type="image/x-ico">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 
 <body>
 
@@ -16,14 +17,15 @@
         <!--  bienvenida   -->
 <div class="alert alert-dark text-center">
         <h1>BIENVENIDO A LA PAGINA DE ACTIVIDADES</h1>
+
 </div>
 
 <div class="row">
-    <div class="col-md-8 offset-md-1">
-        <h2 class="has-text-aling-left">
+    <div class="col-md-12">
+        <h2 class="has-text-aling-left text-center">
             <strong>Eventos Disponibles actualmente</strong>
         </h2>
-        <p class="has-text-aling-left">
+        <p class="has-text-aling-left text-center">
             "Hecha un vistazo a nuestro catalogo de eventos disponibles a los cuales puedes registrarte, para poder aumentar tus conocimientos"
         </p>
     </div>
@@ -37,8 +39,8 @@
         <div class="col-lg-4 col-md-12">
             <div class="card-body ">
                 <div>
-                    {{$event->estado}}
-                    <div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
+                   
+                    <div class="bg-image hover-overlay ripple card__img" data-mdb-ripple-color="light">
                         @isset($event->imagen)
                             <center>
                                 <img id="img" src="{{Storage::url($event->imagen)}}"  class="img-fluid" alt="">
@@ -49,20 +51,18 @@
                             </div>
                         @endisset
                     </div>
-                    <div><br>
-                        <h5 class="card-title text-center">
+                    <div class="card__body">
+                        <h5 class="card-title text-center card__title">
                             {{$event->evento}}
                         </h5>
-                        <p class="card-text">
                             {!!$event->detalle!!}
-                        </p>
                     </div>
                 </div>
                 <div class="card-text text-center">
 
                     @if($date > $event->fecha_fin)
                         
-                        <h4>Evento Culminado</h4>
+                        <h4 class="txt__evento-culminado">Evento Culminado</h4>
                     @else 
                         @if ($event->costo_student > 0 && $event->costo_prof > 0)
                             <h6>Costo para Estudiantes: {{$event->costo_student}}</h6>
@@ -70,7 +70,7 @@
                             <h6>Costo para Profesionales: {{$event->costo_prof}}</h6>
                             
                         @else
-                            <h5>Evento gratuito</h5>
+                            <h5 class="txt__evento-gratuito">Evento gratuito</h5>
                         @endif
                         
                         <h6>Fecha de inicio: {{$event->fecha_inicio}}</h6>
@@ -80,7 +80,7 @@
                     
                     @endif
                     <br><br>
-                    <a href="{{route('events.show', $event)}}" class="btn btn-primary">Ver evento</a>
+                    <a href="{{route('events.show', $event)}}" class="btn btn-outline-dark">Ver evento</a>
                     {{-- {{ $event->id }} --}}
 
                 </div>
@@ -96,7 +96,43 @@
 </div>
 <!-- Fin de la seccion -->
 
+<style>
+        .card__img{
+            height: 200px;
+            overflow: hidden;
+            object-fit: cover;
+            object-position: center;
+        }
+        .txt__evento-culminado{
+            color:#ff0000;
+        }
+        .txt__evento-gratuito{
+            color: #008000;
+        }
+        .card__title{
+            height: 80px;
+            text-align: center;
+            justify-content: center;
+            padding: 20px;
+        }
+        /* .card__text{
+            height: 150px
+        } */
+        .card__body *{
+            display: flex;
+            flex-direction: column;
+            text-align: justify;
+            align-items: center;
+            justify-content: center;
 
+        }
+        .card__body p:first-child{
+            display: none;
+            height: 0;
+            width: 0;
+        }
+       
+</style>
 
 </body>
     @extends('layouts.footer')
